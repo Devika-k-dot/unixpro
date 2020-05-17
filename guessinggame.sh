ae5a39e9e38e5ba2e89bc88ade77a5c67cdf0738
Devika-k-dot <65444577+Devika-k-dot@users.noreply.github.com>
2020-05-17 17:52:01 +0530

    Update guessinggame.sh

--- guessinggame.sh
+++ guessinggame.sh
@@ -4,6 +4,7 @@ while true_a
 do
 echo "guess how many files can be inside the directory"
 read num
+flag=0
 if [ $num -lt $true_a ]
 then
 echo "guess is less"
@@ -12,6 +13,7 @@ then
 echo "guess is greater"
 else
 echo "guess is correct"
+flag=1
 break;
 fi
 done
