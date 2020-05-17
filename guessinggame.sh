9c5840edf5feb2d91fec34ad2c6720c1805f2f80
Devika-k-dot <65444577+Devika-k-dot@users.noreply.github.com>
2020-05-17 17:30:39 +0530

    Update guessinggame.sh

--- guessinggame.sh
+++ guessinggame.sh
@@ -1,8 +1,8 @@
 function guessing(){
     true_a=$(ls -l |grep "^-"|wc -l)
-while true
+while true_a
 do
-echo "guess"
+echo "guess how many files can be inside the directory"
 read num
 if [ $num -lt $true_a ]
 then
